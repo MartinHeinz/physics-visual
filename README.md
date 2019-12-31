1f80fd0a470f009a0ab069e58f8c441083377474
MartinHeinz <martin7.heinz@gmail.com>
2019-12-31 12:26:07 +0100

    Docs and README.md.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Realistic 2D Physics with (Plain/Vanilla) JavaScript
+
+This project is a simple simulation/visualization of some physics concepts like gravity, collisions or acceleration in 2D.
+
+## Live Demo
+
+Live demo is available at <https://martinheinz.github.io/physics-visual/>
+
+You can create new objects by clicking and holding left mouse button - the longer you hold the bigger the objects will be.
+
+There are preconfigured example which you can use by clicking on buttons in top right.
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+/** Describes object (circle) drawn on canvas and its attributes. */
 class Shape {
     constructor(x, y, radius, ax, ay, m, vx=0, vy=0) {
         this.x = x;
@@ -31,6 +32,7 @@ class Shape {
         this.x += this.vx * dt;
         this.y += this.vy * dt;
     }
+
     draw() {
         //draw a circle
         ctx.beginPath();
@@ -71,7 +73,7 @@ class Shape {
     }
 }
 
-
+/** Object describing collision between 2 objects */
 class Collision {
     constructor(o1, o2, dx, dy, d) {
         this.o1 = o1;
@@ -105,16 +107,18 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function resolveCollision(info) {
-    let nx = info.dx /info.d;
+/** Resolves collision by pushing objects away from each other. */
+function resolveCollision(info) {  // "info" is a Collision object from above
+    let nx = info.dx /info.d;  // Compute iegen vectors
     let ny = info.dy /info.d;
     let s = info.o1.r + info.o2.r - info.d;
-    info.o1.x -= nx * s/2;
+    info.o1.x -= nx * s/2;  // Move first object by half of collision size
     info.o1.y -= ny * s/2;
-    info.o2.x += nx * s/2;
+    info.o2.x += nx * s/2;  // Move other object by half of collision size in opposite direction
     info.o2.y += ny * s/2;
 }
 
+/** Resolves collision by bouncing objects. */
 function resolveCollisionWithBounce(info) {
     let nx = info.dx /info.d;
     let ny = info.dy /info.d;
@@ -124,51 +128,49 @@ function resolveCollisionWithBounce(info) {
     info.o2.x += nx * s/2;
     info.o2.y += ny * s/2;
 
+    // Magic...
     let k = -2 * ((info.o2.vx - info.o1.vx) * nx + (info.o2.vy - info.o1.vy) * ny) / (1/info.o1.m + 1/info.o2.m);
-    info.o1.vx -= k * nx / info.o1.m;
+    info.o1.vx -= k * nx / info.o1.m;  // Same as before, just added "k" and switched to "m" instead of "s/2"
     info.o1.vy -= k * ny / info.o1.m;
     info.o2.vx += k * nx / info.o2.m;
     info.o2.vy += k * ny / info.o2.m;
 }
 
-function moveWithGravity(dt, o) {
-    for (let o1 of o) {
+function moveWithGravity(dt, o) {  // "o" refers to Array of objects we are moving
+    for (let o1 of o) {  // Zero-out accumulator of forces for each object
         o1.fx = 0;
         o1.fy = 0;
     }
-
-    for (let [i, o1] of o.entries()) {
+    for (let [i, o1] of o.entries()) {  // For each pair of objects...
         for (let [j, o2] of o.entries()) {
-            if (i < j) {
-                let dx = o2.x - o1.x;
+            if (i < j) {  // To not do same pair twice
+                let dx = o2.x - o1.x;  // Compute distance between centers of objects
                 let dy = o2.y - o1.y;
                 let r = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-                if (r < 1) {
+                if (r < 1) {  // To avoid division by 0
                     r = 1;
                 }
-                let f = (1000 * o1.m * o2.m) / Math.pow(r, 2);
-                let fx = f * dx / r;
+                let f = (1000 * o1.m * o2.m) / Math.pow(r, 2);  // Compute force for this pair
+                let fx = f * dx / r;  // Break it down
                 let fy = f * dy / r;
-                o1.fx += fx;
+                o1.fx += fx;  // Accumulate for first object
                 o1.fy += fy;
-                o2.fx -= fx;
+                o2.fx -= fx;  // And for second object in opposite direction
                 o2.fy -= fy;
             }
         }
     }
-    for (let o1 of o) {
-        let ax = o1.fx / o1.m;
+    for (let o1 of o) {  // for each object update...
+        let ax = o1.fx / o1.m;  // ...acceleration
         let ay = o1.fy / o1.m;
 
-        o1.vx += ax * dt;
+        o1.vx += ax * dt;  // ...speed
         o1.vy += ay * dt;
 
-        o1.x += o1.vx * dt;
+        o1.x += o1.vx * dt;  // ...position
         o1.y += o1.vy * dt;
     }
-
 }
-
 
 const CollisionTypes = Object.freeze({
     "push": resolveCollision,
@@ -282,6 +284,7 @@ document.getElementById("gravityExample").onclick = createGravityExample;
 document.getElementById("bounceExample").onclick = createBouncingExample;
 
 
+/** Used with click + hold events to create circles (objects). */
 function createShape(event, radius=10, mass=100) {
     let x = event.pageX - c.offsetLeft;
     let y = event.pageY - c.offsetTop;
@@ -294,12 +297,11 @@ let timerFlag;
 let startTime = new Date();
 function mouseDown() {
     startTime = new Date();
-    holdTimer = window.setTimeout(myFunction, 100); //set timeout to fire in 2 seconds when the user presses mouse button down
+    holdTimer = window.setTimeout(function() {
+        timerFlag = true;
+        }, 100);
 }
 
-function myFunction() {
-    timerFlag = true;
-}
 c.addEventListener("mousedown", mouseDown);
 c.addEventListener("mouseup", function(event) {
     removeTimer(event)
@@ -320,7 +322,8 @@ function removeTimer(event) {
     timerFlag = false;
 }
 
-
+/** This function is ran with every animation frame and each time clears canvas, updates coordinates of all objects,
+ * resolves collisions of objects and edges of canvas , resolves collisions between objects and finally draws all of them. */
 function animate() {
     ctx.clearRect(0, 0, c.width, c.height);
 
